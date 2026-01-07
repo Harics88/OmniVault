0ce7b32196615d665f007ed72319d4cbd24f13c7
HPG <hpdev888@gmail.com>
2026-01-07 06:43:33 -0500

    Upgrade to Tauri v2 and fix configuration format

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
